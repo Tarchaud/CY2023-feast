4dab711d91e7a86550ff8cfa8c97b2cd7461e4e9
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2023-11-06 14:11:02 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { MenuComponent } from './template/menu/menu.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { UsersComponent } from './pages/users/users.component';
+import { ProfilComponent } from './pages/profil/profil.component';
+import { EvenementsComponent } from './pages/evenements/evenements.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    AccueilComponent,
+    UsersComponent,
+    ProfilComponent,
+    EvenementsComponent,
+    InscriptionComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
